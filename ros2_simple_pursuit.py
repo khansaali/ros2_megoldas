1d149fb30a599c61e4b187e5953b9b11eacedc09
khansaali <136676002+khansaali@users.noreply.github.com>
2024-03-07 20:24:06 +0100

    Update ros2_simple_pursuit.py

--- ros2_simple_pursuit.py
+++ ros2_simple_pursuit.py
@@ -5,3 +5,11 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist, Point, PointStamped, Transform
 from visualization_msgs.msg import Marker
 import std_msgs
+KOZEPISKOLA_NEVE = "Ismeretlen kozepiskola"
+KOZEPISKOLA_AZON = "A00"
+ANGLE_RANGE = 360 # LSN10 LIDAR has 360 degrees scan
+DESIRED_DISTANCE_RIGHT = 1.0 #0.9 # meters
+DESIRED_DISTANCE_LEFT = 0.8 # 0.55
+VELOCITY = 1.00 # meters per second
+CAR_LENGTH = 0.445 # 0.445 meters
+WHEELBASE = 0.3187; # documention based | measured: ~32 cm
