bba9afeaf9b55599ceaa597649906040ae432e54
khansaali <136676002+khansaali@users.noreply.github.com>
2024-03-07 21:37:42 +0100

    Update ros2_simple_pursuit.py

--- ros2_simple_pursuit.py
+++ ros2_simple_pursuit.py
@@ -1,4 +1,4 @@
-lfrom __future__ import print_function
+from __future__ import print_function
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
@@ -219,7 +219,57 @@ def getAngle(self,ranges, angles):
         
         # Publish the Twist message to the "cmd_vel" topic
         self.pub.publish(msg_cmd)   
+    if __name__ == '__main__':
+    first_run = True
+    rclpy.init('dist_finder',anonymous = True)
+    rclpy.create_subscription("scan",LaserScan,callbackLaser)
+    rclpy.logging("Simple pursuit node started")
+    """
+    ros2 run tf tf_echo base_link laser
+    - Translation: [0.260, 0.000, 0.228]
+    - Rotation: in Quaternion [0.000, 0.000, 1.000, 0.001]
+                in RPY (radian) [0.000, -0.000, 3.140]
+                in RPY (degree) [0.000, -0.000, 179.909]
+    header:
+        frame_id: "base_link"
+    child_frame_id: "laser"
+    transform:
+        translation:
+            x: 0.26
+            y: 0.0
+            z: 0.228
+        rotation:
+            x: 0.0
+            y: 0.0
+            z: 0.999999682932
+            w: 0.000796326710733
+    """
+    rate = rclpy.create_timer(2) # 2hz
+    buf = tf2_ros.Buffer()
+    listener = tf2_ros.tf2_ros.BufferClient(buf)
 
+    while not rclpy.is_shutdown():
+        if first_run:
+            try:
+                trans = buf.lookup_transform("base_link", "laser", rclpy.Time())
+                rclpy.logging("Got laser fransform")
+                first_run = False
+                #rclpy.logging(trans)
+            except:
+                self.trans.translation.x = 0.26
+                self.trans.translation.y = 0.0
+                self.trans.translation.z = 0.228
+                self.trans.rotation.x = 0.0
+                self.trans.rotation.y = 0.0
+                self.trans.rotation.z = 0.999999682932
+                self.trans.rotation.w = 0.000796326710733
+                #rclpy.logging(trans)
+                #rclpy.logging("No transform to laser assuming original!")
+        self.pubst2.publish(KOZEPISKOLA_NEVE + "(" + KOZEPISKOLA_AZON + ")")
+        try:
+            rate.sleep()
+        except rclpy.exceptions.ROSInterruptException:
+            pass
 
 
 
