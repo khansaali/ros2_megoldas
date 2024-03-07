cac25b05e25a92bc38dc40d96bb2a9892d09b5b1
khansaali <136676002+khansaali@users.noreply.github.com>
2024-03-07 20:22:30 +0100

    Create ros2_simple_pursuit.py

---
+++ ros2_simple_pursuit.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Point, PointStamped, Transform
+from visualization_msgs.msg import Marker
+import std_msgs
